0c96557a5deba3fd509fcb8d37a39ae15dcf9f24
Steelnox <eric14pastor@gmail.com>
2020-12-30 10:56:57 +0100

    Milestone 3 realizada

--- UD6_M2/UD6_M2/Program.cs
+++ UD6_M2/UD6_M2/Program.cs
@@ -8,6 +8,8 @@ namespace UD6_M2
         {
             Milestone1 m1 = new Milestone1();
             Milestone2 m2 = new Milestone2();
+            Milestone3 m3 = new Milestone3();
+
 
             string opcion = "";
             Console.WriteLine("Seleccione una opcion de la 1 a la 3");
@@ -20,6 +22,9 @@ namespace UD6_M2
                     break;
                 case "2":
                     m2.Principal();
+                    break;
+                case "3":
+                    m3.Reloj();
                     break;
                 default:
                     Console.WriteLine("No existe esa opcion. Intentalo de nuevo más tarde");
