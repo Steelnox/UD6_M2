f14fc24daf3ccc9aaf1b127aa444308a9de5529f
Steelnox <eric14pastor@gmail.com>
2020-12-30 10:29:31 +0100

    Milestone 2 realizada

---
+++ UD6_M2/UD6_M2/Milestone2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD6_M2
+{
+    public class Milestone2
+    {
+        public void Principal()
+        {
+            int altura = 0;
+            Console.WriteLine("Dame la altura");
+            altura = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= altura; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = altura ; i >= 1; i--)
+            {
+                for (int lado = altura - 1; lado >= i; lado--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- UD6_M2/UD6_M2/Program.cs
+++ UD6_M2/UD6_M2/Program.cs
@@ -7,8 +7,25 @@ namespace UD6_M2
         static void Main(string[] args)
         {
             Milestone1 m1 = new Milestone1();
+            Milestone2 m2 = new Milestone2();
 
-            m1.Principal();
+            string opcion = "";
+            Console.WriteLine("Seleccione una opcion de la 1 a la 3");
+            opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    m1.Principal();
+                    break;
+                case "2":
+                    m2.Principal();
+                    break;
+                default:
+                    Console.WriteLine("No existe esa opcion. Intentalo de nuevo más tarde");
+                    break;
+            }
+
         }
     }
 }
